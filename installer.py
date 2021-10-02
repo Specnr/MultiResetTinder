9f961ad0138709a6950a2b8aed0522cf574aeb4e
Sharpieman20 <>
2021-10-02 10:43:45 -0500

    Restructure repo

---
+++ installer.py
@@ -0,0 +1,13 @@
+
+import requests
+
+get_release_url = 'https://raw.githubusercontent.com/Sharpieman20/MultiResetTinder/main/release.txt'
+
+release_url = requests.get(get_release_url).text.rstrip()
+
+r = requests.get(release_url, allow_redirects=True)
+open('release.zip', 'wb').write(r.content)
+
+import zipfile
+with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
+    zip_ref.extractall(directory_to_extract_to)
