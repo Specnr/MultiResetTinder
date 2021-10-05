fa487a1d9075f28851dffe3a2882a27dde36a506
Sharpieman20 <>
2021-10-05 18:35:02 -0500

    Making a better installer

---
+++ test/installer.py
@@ -0,0 +1,19 @@
+
+import requests
+
+# TODO - make this script also install itself
+
+get_release_url = 'https://raw.githubusercontent.com/Sharpieman20/MultiResetTinder/main/release.txt'
+
+release_url = requests.get(get_release_url).text.rstrip()
+
+r = requests.get(release_url, allow_redirects=True)
+open('setup.py', 'wb').write(r.content)
+
+import setup
+
+print('done')
+
+# import zipfile
+# with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
+#     zip_ref.extractall("src")
